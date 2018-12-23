fca469069cecfe45b7fb0cce698bff270062be65
yuanqiangwang <yqcolin@gmail.com>
2018-12-23 20:56:23 +0800

    Rebuild By NetStandard 2.0

--- src/Log4Mongo/BackwardCompatibility.cs
+++ src/Log4Mongo/BackwardCompatibility.cs
@@ -1,78 +1,78 @@
-﻿using System;
-using System.Collections;
-using System.Text;
-using MongoDB.Bson;
-using MongoDB.Driver;
-using log4net.Core;
+﻿using log4net.Core;
 using log4net.Util;
+using MongoDB.Bson;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Log4Mongo
 {
-	public class BackwardCompatibility
-	{
-		public static BsonDocument BuildBsonDocument(LoggingEvent loggingEvent)
-		{
-			if(loggingEvent == null)
-			{
-				return null;
-			}
+    public class BackwardCompatibility
+    {
+        public static BsonDocument BuildBsonDocument(LoggingEvent loggingEvent)
+        {
+            if (loggingEvent == null)
+            {
+                return null;
+            }
 
-			var toReturn = new BsonDocument {
-				{"timestamp", loggingEvent.TimeStamp}, 
-				{"level", loggingEvent.Level.ToString()}, 
-				{"thread", loggingEvent.ThreadName}, 
-				{"userName", loggingEvent.UserName}, 
-				{"message", loggingEvent.RenderedMessage}, 
-				{"loggerName", loggingEvent.LoggerName}, 
-				{"domain", loggingEvent.Domain}, 
-				{"machineName", Environment.MachineName}
-			};
+            var toReturn = new BsonDocument {
+                {"timestamp", loggingEvent.TimeStamp},
+                {"level", loggingEvent.Level.ToString()},
+                {"thread", loggingEvent.ThreadName},
+                {"userName", loggingEvent.UserName},
+                {"message", loggingEvent.RenderedMessage},
+                {"loggerName", loggingEvent.LoggerName},
+                {"domain", loggingEvent.Domain},
+                {"machineName", Environment.MachineName}
+            };
 
-			// location information, if available
-			if(loggingEvent.LocationInformation != null)
-			{
-				toReturn.Add("fileName", loggingEvent.LocationInformation.FileName);
-				toReturn.Add("method", loggingEvent.LocationInformation.MethodName);
-				toReturn.Add("lineNumber", loggingEvent.LocationInformation.LineNumber);
-				toReturn.Add("className", loggingEvent.LocationInformation.ClassName);
-			}
+            // location information, if available
+            if (loggingEvent.LocationInformation != null)
+            {
+                toReturn.Add("fileName", loggingEvent.LocationInformation.FileName);
+                toReturn.Add("method", loggingEvent.LocationInformation.MethodName);
+                toReturn.Add("lineNumber", loggingEvent.LocationInformation.LineNumber);
+                toReturn.Add("className", loggingEvent.LocationInformation.ClassName);
+            }
 
-			// exception information
-			if(loggingEvent.ExceptionObject != null)
-			{
-				toReturn.Add("exception", BuildExceptionBsonDocument(loggingEvent.ExceptionObject));
-			}
+            // exception information
+            if (loggingEvent.ExceptionObject != null)
+            {
+                toReturn.Add("exception", BuildExceptionBsonDocument(loggingEvent.ExceptionObject));
+            }
 
-			// properties
-			PropertiesDictionary compositeProperties = loggingEvent.GetProperties();
-			if(compositeProperties != null && compositeProperties.Count > 0)
-			{
-				var properties = new BsonDocument();
-				foreach(DictionaryEntry entry in compositeProperties)
-				{
-					properties.Add(entry.Key.ToString(), entry.Value.ToString());
-				}
+            // properties
+            PropertiesDictionary compositeProperties = loggingEvent.GetProperties();
+            if (compositeProperties != null && compositeProperties.Count > 0)
+            {
+                var properties = new BsonDocument();
+                foreach (DictionaryEntry entry in compositeProperties)
+                {
+                    properties.Add(entry.Key.ToString(), entry.Value.ToString());
+                }
 
-				toReturn.Add("properties", properties);
-			}
+                toReturn.Add("properties", properties);
+            }
 
-			return toReturn;
-		}
+            return toReturn;
+        }
 
-		private static BsonDocument BuildExceptionBsonDocument(Exception ex)
-		{
-			var toReturn = new BsonDocument {
-				{"message", ex.Message}, 
-				{"source", ex.Source}, 
-				{"stackTrace", ex.StackTrace}
-			};
+        private static BsonDocument BuildExceptionBsonDocument(Exception ex)
+        {
+            var toReturn = new BsonDocument {
+                {"message", ex.Message},
+                {"source", ex.Source},
+                {"stackTrace", ex.StackTrace}
+            };
 
-			if(ex.InnerException != null)
-			{
-				toReturn.Add("innerException", BuildExceptionBsonDocument(ex.InnerException));
-			}
+            if (ex.InnerException != null)
+            {
+                toReturn.Add("innerException", BuildExceptionBsonDocument(ex.InnerException));
+            }
 
-			return toReturn;
-		}
-	}
+            return toReturn;
+        }
+    }
 }
--- src/Log4Mongo/MongoAppenderFileld.cs
+++ src/Log4Mongo/MongoAppenderFileld.cs
@@ -1,10 +1,13 @@
 ﻿using log4net.Layout;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Log4Mongo
 {
-	public class MongoAppenderFileld
-	{
-		public string Name { get; set; }
-		public IRawLayout Layout { get; set; }
-	}
+    public class MongoAppenderFileld
+    {
+        public string Name { get; set; }
+        public IRawLayout Layout { get; set; }
+    }
 }
